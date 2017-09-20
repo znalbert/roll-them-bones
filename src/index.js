a091260787d577f168c1f87955bc95fc6c4ea83a
Zoltan Albert <znalbert@gmail.com>
2017-09-20 13:18:04 -0500

    Add ESLint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const body = document.getElementsByTagName('body');
-const greet = document.createElement('div')
-const greeting = 'Hello, Multiverse.'
+const greet = document.createElement('div');
+const greeting = 'Hello, Multiverse.';
 
 greet.textContent = greeting;
 body[0].appendChild(greet);
